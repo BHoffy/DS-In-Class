925463c9e20c20c5525cea671461be0c95d3895c
BHoffy <brandon.hoffman360@yahoo.com>
2021-09-15 14:01:35 -0400

    class work from today

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,7 @@
 const Offer = {
     data() {
       return {
+        "result": {},
         "offers": [
                 {
                     "id": 1,
@@ -21,7 +22,23 @@ const Offer = {
                 }
             ]
         }
-    }
-  }
-  
-Vue.createApp(Offer).mount('#offerApp') 
+    },
+    created() {
+      console.log("A");
+      fetch('https://randomuser.me/api/')
+      .then( response => response.json() )
+      .then ( (responseJson => {
+          console.log(responseJson);
+          console.log("C")
+          this.result = responseJson.results[0];
+      })
+      .catch( (err)  => {
+        console.error(err);
+            
+        console.log("B");
+      })
+      )}
+}
+ 
+Vue.createApp(Offer).mount('#offerApp')
+console.log("Z");
