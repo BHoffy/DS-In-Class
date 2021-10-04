ffeacb8b09bdfa8687d7c5f4d3a64320e9318663
BHoffy <brandon.hoffman360@yahoo.com>
2021-10-04 11:33:05 -0400

    lol

--- app/public/api/test.php
+++ app/public/api/test.php
@@ -8,13 +8,13 @@ echo $foo ." ". $bar ."\n";
 
 echo $num * $num * $num;
 
-//This is a comment
+// This is a comment
+# This is also a comment
 
-# comment
-
-/* multi
-line comment */
-
+/* This is a
+multi-line
+comment
+*/
 
 if (true) {
     echo "TRUE \n";
@@ -29,24 +29,23 @@ while (true) {
 $arr = [1,1,2,3,5,8];
 
 $arr2 = [
-    "first" => "Tom",
+    "first"  => "Tom",
     "second" => "Bipin",
-    "best" => "DS"
+    "best"   => "DS"
 ];
 
 function printList($someArr) {
-
-echo "<ul>\n";
-foreach($someArr as $key => $val) {
-    echo "<li>".$key." is ".$val."</li>\n";
-}
-echo "</ul>\n";
-
+    echo "<ul>\n";
+    foreach($someArr as $key => $val) {
+        echo "<li>".$key." is ".$val."</li>\n";
+    }
+    echo "</ul>\n";
 }
 
 printList($arr);
 printList($arr2);
 
-
-echo json_encode ($arr2, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR
+echo json_encode(
+    $arr2,
+    JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR
 );
